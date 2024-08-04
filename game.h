1baf7fea29947821f4b6ce3a1e30078c60c9cc45
Krishna kant sharma <116259285+krishnactive@users.noreply.github.com>
2024-08-04 12:52:43 +0530

    game.h

--- game.h
+++ game.h
@@ -6,11 +6,14 @@
 #define DOWN -1
 #define RIGHT 2
 #define LEFT -2
+#define MAX 60
+
+
 void initGrid(int,int);
 void drawGrid();
 void unit(int,int);
 void drawSnake();
-
-
+void drawFood();
+void random(int&, int&);
 
 #endif // GAME_H_INCLUDED
